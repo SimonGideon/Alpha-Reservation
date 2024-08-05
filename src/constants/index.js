0a07818147bb01eb36a695486381ab04d480865d
SimonGideon <simongideon918@gmail.com>
2024-08-06 00:00:43 +0300

    Add live link

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,12 +1,13 @@
+/* eslint-disable quotes */
 const links = [
-  { path: '/home', text: 'Houses' },
-  { path: '/reserve', text: 'Reserve' },
-  { path: '/myReservations', text: 'My Reservations' },
-  { path: '/addHouse', text: 'Add House' },
-  { path: '/deleteHouse', text: 'Delete House' },
-  { path: '/logout', text: 'Logout' },
+  { path: "/home", text: "Houses" },
+  { path: "/reserve", text: "Reserve" },
+  { path: "/myReservations", text: "My Reservations" },
+  { path: "/addHouse", text: "Add House" },
+  { path: "/deleteHouse", text: "Delete House" },
+  { path: "/logout", text: "Logout" },
 ];
 
-const backendLink = 'https://book-an-appointment-backend-3kmv.onrender.com/';
+const backendLink = "https://alpha-reservation-a6eafb83816b.herokuapp.com/";
 
 export { links, backendLink };
